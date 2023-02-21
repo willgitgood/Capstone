d2f6a2dffaa905bc0b6346c11c862014bec86dec
William Pruden <willpruden@gmail.com>
2023-02-20 19:41:52 -0500

    new push

---
+++ components/Footer.js
@@ -0,0 +1,6 @@
+import html from "html-literal";
+
+export default () => html`
+<footer>
+&copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
+</footer>`;
--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+  ${Header(state)}
+  ${Nav(store.Links)}
+  ${Main(state)}
+  ${Footer()}
+  `;
+  afterRender();
+  router.updatePageLinks();
+}
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    }
+  })
+  .resolve();
