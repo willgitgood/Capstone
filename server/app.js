4432fc333c979711701a878471c306e95ce30d62
William Pruden <willpruden@gmail.com>
2023-03-07 21:32:19 -0500

    working spa

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
@@ -22,16 +23,31 @@ const logging = (request, response, next) => {
   next();
 };
 
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
 app.use(express.json());
 app.use(logging);
 
-
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
-   // Create the headers for response by default 200
-   // Create the response body
-   // End and return the response
-   response.status(418).json({ message: "Service healthy" });
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.status(418).json({ message: "Service healthy" });
 });
 
 app.get("/weather/:city", (request, response) => {
@@ -54,15 +70,13 @@ app.post("/add", (request, response) => {
   const num1 = request.body.numberOne;
   const num2 = request.body.numberTwo;
   const responseBody = {
-     sum: num1 + num2
+    sum: num1 + num2
   };
   response.json(responseBody);
 });
 
+app.use("/pizzas", pizzas);
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
 // always be last
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-
-
--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -1,9 +1,61 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const Pizza = require("../models/Pizza");
 const router = Router();
 
+// Create record in MongoDB Atlas using Mongoose.js ORM
+router.post("/", (request, response) => {
+  const newPizza = new Pizza(request.body);
+  newPizza.save((error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
 
+// Get (read) all records from the collection
+router.get("/", (request, response) => {
+  Pizza.find({}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
 
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+  Pizza.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
 
+router.delete("/:id", (request, response) => {
+  Pizza.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Pizza.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        // Take note that the customer is not included, so it can't
+        crust: body.crust,
+        cheese: body.cheese,
+        sauce: body.sauce,
+        toppings: body.toppings
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
 
 module.exports = router;
