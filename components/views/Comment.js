a107f48dc6e02dc5315d064e352ed2ea7c11c7cc
William Pruden <willpruden@gmail.com>
2023-03-19 19:58:51 -0400

    new changes

--- components/views/Comment.js
+++ components/views/Comment.js
@@ -2,20 +2,42 @@ import html from "html-literal";
 
 export default state => html`
   <section id="comment">
-    <!-- form for name -->
-    <form method="GET">
-      <label
-        >Name:
-        <input name="submitted-name" autocomplete="name" />
-      </label>
-      <button id="namebutton">Save</button>
-    </form>
-    <form method="GET">
-      <label
-        >Affirmation:
-        <input name="Affirmation" />
-      </label>
-      <button id="namebutton">Save</button>
-    </form>
+    <!-- form -->
+  <form method="POST" action="https://formspree.io/f/xnqybqbg">
+    <!-- name -->
+        <div id="submittedname">
+        <label for="submittedname"
+          >Name:</label>
+          <input name="submitted-name" autocomplete="name" type="text" id="submittedname" required/>
+          </div>
+          <!-- affirmation -->
+        <div id="affirmation"></div>
+        <label for="affirmation"
+          >Affirmation:
+          <input type="text" name="affirmation"  id="affirmation" required />
+        </label>
+        </div>
+        <!-- <button id="namebutton">
+          Save
+        </button> -->
+        <div id="submitbutton">
+        <input type="submit" value="Submit">
+        </div>
+      </form>
   </section>
+
+  <!-- get comments -->
+  <div id="affirmationcomment">
+    <table id="comments">
+      <tr>
+        <th>Name:</th>
+      </tr>
+      <tr>
+        <th>Affirmation:</th>
+      </tr>
+
+    </table>
+
+
+  </div>
 `;
--- server/app.js
+++ server/app.js
@@ -1,16 +1,9 @@
-// 'Import' the Express module instead of http
 const express = require("express");
-// Initialize the Express application
 const app = express();
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
-  // Create the headers for response by default 200
-  // Create the response body
-  // End and return the response
   response.send(JSON.stringify({ message: "Service healthy" }));
 });
 
-// Tell the Express app to start listening
-// Let the humans know I am running and listening on 4040
 app.listen(4040, () => console.log("Listening on port 4040"));
