324f3e290540e462575a024de0cb3bbf27178748
William Pruden <91891730+NealSpits@users.noreply.github.com>
2023-03-23 16:32:51 +0000

    changes

---
+++ server/router/comment.js
@@ -0,0 +1,59 @@
+const { Router } = require("express");
+const Comments= require("./models/Comments");
+const router = Router();
+
+// Create record in MongoDB Atlas using Mongoose.js ORM
+router.post("/", (request, response) => {
+  const newComment = new Comments(request.body);
+  newComment.save((error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get (read) all records from the collection
+router.get("/", (request, response) => {
+  Comments.find({}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+  Comments.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.delete("/:id", (request, response) => {
+  Comments.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Comments.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        // Take note that the customer is not included, so it can't
+        crust: body.crust,
+        cheese: body.cheese,
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
+module.exports = router;
